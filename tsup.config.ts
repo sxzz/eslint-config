50628e8142f86dcd0fddb5630c536b9b7d7e8767
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-04-25 01:23:37 +0800

    refactor!: drop cjs package

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
+  format: ['esm'],
   target: 'node18.18',
   dts: true,
 })
