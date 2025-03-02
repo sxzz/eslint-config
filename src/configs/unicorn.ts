ab2950784a8b8e97105dd82949ac286061d2c7af
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-03-03 01:22:16 +0800

    chore: fix lint

--- src/configs/unicorn.ts
+++ src/configs/unicorn.ts
@@ -9,6 +9,7 @@ export const unicorn = (): Config[] => [
     },
     rules: {
       'unicorn/catch-error-name': 'error',
+      'unicorn/consistent-date-clone': 'error',
       'unicorn/consistent-empty-array-spread': 'error',
       'unicorn/consistent-existence-index-check': 'error',
       'unicorn/consistent-function-scoping': [
@@ -37,7 +38,6 @@ export const unicorn = (): Config[] => [
       'unicorn/no-for-loop': 'error',
       'unicorn/no-hex-escape': 'error',
       'unicorn/no-instanceof-builtins': 'error',
-      'unicorn/consistent-date-clone': 'error',
       'unicorn/no-invalid-remove-event-listener': 'error',
       'unicorn/no-lonely-if': 'error',
       'unicorn/no-negation-in-equality-check': 'error',
