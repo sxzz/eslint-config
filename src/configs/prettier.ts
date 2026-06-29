906b3f88ed9b67ef4d6bcb78d9c313cb70962323
Kevin Deng <sxzz@sxzz.moe>
2026-06-29 13:35:47 +0800

    fix(unicorn): add files filter

--- src/configs/prettier.ts
+++ src/configs/prettier.ts
@@ -2,6 +2,7 @@ import { pluginPrettier, pluginPrettierRecommended } from '../plugins.ts'
 import type { Config } from '../types.ts'
 
 const rules = { ...pluginPrettierRecommended.rules }
+// eslint-disable-next-line unicorn/no-top-level-side-effects
 delete rules['vue/html-self-closing']
 
 export const prettier = (): Config[] => [
